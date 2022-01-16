9c542bb71fbce4a9a1801819e495d015495ee00b
Goncalooliv <38600@ufp.edu.pt>
2022-01-16 17:04:05 +0000

    mais 1 voltinha

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,13 +4,14 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use PHPUnit\Framework\TestCase;
 
 class LoginTest extends TestCase
 {
     public function test_login()
     {
-        $user = factory(User::class)->create();
+        $user = User::factory()->create();
 
         $responde = $this->actingAs($user)->withSession(['foo' => 'bar'])->get('/home');
     }
