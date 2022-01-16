95e4a1fb23429b5b49c1194248808da9a5ef8d1b
Goncalooliv <38600@ufp.edu.pt>
2022-01-16 17:01:16 +0000

    mais 1

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -8,11 +8,10 @@ use PHPUnit\Framework\TestCase;
 
 class LoginTest extends TestCase
 {
-    public function test_if_login(){
-        $response=$this->post('login',[
-            'email'=>'38600@ufp.edu.pt',
-            'password'=>'12345678',
-        ]);
-        $response->assertRedirect('/home');
+    public function test_login()
+    {
+        $user = factory(User::class)->create();
+
+        $responde = $this->actingAs($user)->withSession(['foo' => 'bar'])->get('/home');
     }
 }
